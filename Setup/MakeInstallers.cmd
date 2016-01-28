684a9d2f386b529a8e0bd64a3699be7ab583004d
EbenZhang <zhangyingneng@gmail.com>
2016-01-28 21:36:42 +0800

    Increase ver

--- Setup/MakeInstallers.cmd
+++ Setup/MakeInstallers.cmd
@@ -6,8 +6,8 @@ rem
 rem Update this version number with every release
 rem
 setlocal
-set version=2.49.99.5
-set numericVersion=2.49.99.5
+set version=2.49.99.6
+set numericVersion=2.49.99.06
 
 set normal=GitExtensions-%Version%-Setup.msi
 set complete=GitExtensions-%Version%-SetupComplete.msi
