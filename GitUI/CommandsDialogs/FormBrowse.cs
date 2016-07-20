b4fd27e318741a3961c43c9c6190ba1a718c8c05
EbenZhang <zhangyingneng@gmail.com>
2016-07-20 22:42:22 +0800

    Recover Splitter Container Layout
    (cherry picked from commit 5ded71cc00c7694074ac4a5c4ec4a8344c6278f9)

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -167,6 +167,7 @@ namespace GitUI.CommandsDialogs
         {
             InitializeComponent();
             Translate();
+            RecoverSplitterContainerLayout();
         }
 
         public FormBrowse(GitUICommands aCommands, string filter)
@@ -259,7 +260,8 @@ namespace GitUI.CommandsDialogs
             RevisionGrid.MenuCommands.MenuChanged += (sender, e) => _formBrowseMenus.OnMenuCommandsPropertyChanged();
             SystemEvents.SessionEnding += (sender, args) => SaveApplicationSettings();
 
-			FillTerminalTab();
+            FillTerminalTab();
+            RecoverSplitterContainerLayout();
         }
 
         private new void Translate()
@@ -3629,6 +3631,13 @@ namespace GitUI.CommandsDialogs
         private void toolStripBranches_DropDown_ResizeDropDownWidth (object sender, EventArgs e)
         {
             ComboBoxHelper.ResizeComboBoxDropDownWidth (toolStripBranchFilterComboBox.ComboBox, AppSettings.BranchDropDownMinWidth, AppSettings.BranchDropDownMaxWidth);
+        }
+
+        private void RecoverSplitterContainerLayout()
+        {
+            var settings = Properties.Settings.Default;
+            FileTreeSplitContainer.SplitterDistance = settings.FormBrowse_FileTreeSplitContainer_SplitterDistance;
+            DiffSplitContainer.SplitterDistance = settings.FormBrowse_DiffSplitContainer_SplitterDistance;
         }
     }
 }
